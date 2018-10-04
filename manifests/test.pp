6935ac0be34621fc2477194f840c837a48568904
lmayorga <lmayorga@lm3corp.com>
2018-10-04 17:16:30 -0400

    escape double quotes

--- manifests/test.pp
+++ manifests/test.pp
@@ -2,8 +2,22 @@
 
 $value=lookup('test_value')
 $value2=lookup('test_value_2')
+$value3=lookup('test_value_3')
+
 
 
 notify{ $value: }
 notify{ $value2: }
+
+exec { 'show a command':
+  command   => "echo groups=${value2}",
+  path      => ['/bin','/usr/bin'],
+  logoutput => true
+}
+
+exec { 'show a command 2':
+  command   => "echo groups=${value3}",
+  path      => ['/bin','/usr/bin'],
+  logoutput => true
+}
 
